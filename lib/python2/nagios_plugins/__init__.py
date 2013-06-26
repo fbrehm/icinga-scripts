483d5a0480923b7a708a17d86ba125ebd2f1633f
Frank Brehm <frank.brehm@profitbricks.com>
2013-06-26 10:01:28 +0000

    Added lib/python2/nagios_plugins/__init__.py, lib/python2.6 and lib/python2.7

---
+++ lib/python2/nagios_plugins/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@copyright: © 2010 - 2013 by Frank Brehm, Berlin
+@summary: All modules for nagions plugins
+"""
+
+# vim: fileencoding=utf-8 filetype=python ts=4
